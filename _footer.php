2fb86ed096c127b4f1f1b9acc9fb9270553c9b3c
waleedo0o7 <waleedhd@gmail.com>
2026-04-10 00:51:09 +0200

    add tabs to design system

---
+++ _footer.php
@@ -0,0 +1,17 @@
+
+
+
+
+
+    <!-- jquery -->
+    <script src="vendors/jquery/jquery-4.0.0.min.js"></script>
+
+    <!-- bootstrap JS -->
+    <script src="vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- custom JS -->
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ _header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="vendors/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="style.css" rel="stylesheet">
+
+    <title>Hello, world!</title>
+
+</head>
+
+<body>
